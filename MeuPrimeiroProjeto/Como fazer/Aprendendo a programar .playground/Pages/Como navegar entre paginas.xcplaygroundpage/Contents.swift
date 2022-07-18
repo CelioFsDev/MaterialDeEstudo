6d89be5eec9c6d32028f7a7c205bd0ce0a66cd4f
CelioFS7 <celioferreiramlm@gmail.com>
2022-07-18 14:28:01 -0300

    navegar entre paginas

---
+++ MeuPrimeiroProjeto/Como fazer/Aprendendo a programar .playground/Pages/Como navegar entre paginas.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//    @IBAction func showLogin(_ sender: UIButton) {
+//        self.present(ViewControllerLogin(), animated: true)
+//    }
+    //    @IBAction func showControlerFirst(_ sender: UIButton) {
+    //        navigationController?.pushViewController(ViewControllerRegister(), animated: true)
+    //    }
+        
